591f721d66ff55ba020e41a4777ec77f47a7e587
crispinamuriel <crispinamuriel@gmail.com>
2020-01-28 08:51:30 -0500

    feat: win/lose logic, restart game option, finishing touches

--- backend/ufo-game-crispina/index.js
+++ backend/ufo-game-crispina/index.js
@@ -1,39 +1,41 @@
 const shipArr = require('./ufo');
 
 function getWord() {
-  let fs = require("fs");
-  let text = fs.readFileSync("./nouns.txt", "utf8");
-  let wordsArr = text.split("\n");
-
-  let coinFlip = Math.floor(Math.random() * wordsArr.length);
+  let fs = require('fs');
+  let text = fs.readFileSync('./nouns.txt', 'utf8');
+  let wordsArr = text.split('\n');
+  let coinFlip = Math.floor(Math.random() * wordsArr.length - 1);
   let word = wordsArr[coinFlip];
   return word;
 }
 
 
-console.log(getWord());
-
 class UFO  {
   constructor(word, remainingGuesses) {
     this.word = word.toLowerCase().split('');
-    this.guessed =  [];
-    this.status =  'playing';
-    this.remainingGuesses  =  remainingGuesses;
+    this.guessed = [];
+    this.status = 'playing';
+    this.remainingGuesses = remainingGuesses;
+  }
+
+  welcomeMessage(){
+    console.log('Welcome to Ms.Muriel\'s guessing game! Guess the code word or be abducted!');
+    console.log('Please enter a guess.');
   }
 
   displayStatusMessage() {
-    if(this.status === 'failed') this.loseMessage();
-    if(this.status === 'win') this.winMessage();
-    if(this.status === 'playing') this.currentStatus();
+    if (this.status === 'failed') this.loseMessage();
+    if (this.status === 'win') this.winMessage();
+    if (this.status === 'playing') this.currentStatus();
   }
 
   loseMessage() {
-    console.log('you lose');
     this.currentStatus();
+    console.log('You lose! Say goodbye to earth!');
   }
 
   winMessage() {
-    console.log('you win');
+    console.log(`You guessed the word '${this.word.join('')}'! \n You Win! No aliens for you!`);
   }
 
   currentStatus() {
@@ -44,7 +46,7 @@ class UFO  {
   calculateStatus() {
     const win = this.word.every((letter) => this.guessed.includes(letter));
 
-    if(this.remainingGuesses === 0)  {
+    if (this.remainingGuesses === 0)  {
       this.status = 'failed';
     } else if (win) {
       this.status = 'win';
@@ -60,7 +62,7 @@ class UFO  {
       if(this.guessed.includes(letter)) {
         puzzle += letter;
       } else {
-        puzzle += '_';
+        puzzle += '_ ';
       }
     });
     return puzzle;
@@ -71,36 +73,42 @@ class UFO  {
     const isUnique = !this.guessed.includes(guess);
     const isBadGuess =  !this.word.includes(guess);
 
-    if(this.status !== 'playing')  {
+    if (this.status !== 'playing')  {
       this.calculateStatus();
+      console.log(this.status)
       return
     }
-    if(isUnique) {
+    if (isUnique) {
       this.guessed.push(guess);
     }
-    if(isUnique && isBadGuess) {
+    if (isUnique && isBadGuess) {
       this.remainingGuesses--;
     }
     this.calculateStatus();
+    console.log(this.word);
   }
 }
 
+
 const startGame  = async() => {
+  const readline = require('readline-sync');
   const word = await getWord();
   let game1 = new UFO(word, 6);
-  while (game1.status === "playing") {
+  console.log(shipArr[5]);
+  game1.welcomeMessage();
 
-    console.log(shipArr[5]);
-    for(let i = 0; i < 6; i++){
-      console.log("hi");
-      game1.remainingGuesses--;
-      console.log(game1.remainingGuesses);
-      game1.calculateStatus();
-      // if(game1.remainingGuesses < 2)  game1.status = 'failed'
-    }
+  while (game1.status === 'playing') {
+    console.log(game1.getPuzzle());
+    const guess = readline.question('Type a letter, then press RETURN here: ');
+    game1.makeGuess(guess);
+    console.log('Guesses Remaining: ', game1.remainingGuesses);
+    if (game1.remainingGuesses < 1)  game1.status = 'failed'
+    game1.displayStatusMessage();
   }
+  const play = readline.question('Play again? Type YES or NO: ');
+  if (play.toUpperCase() === 'YES') startGame();
+  if (play.toUpperCase() === 'NO') console.log('Thank you for playing! Goodbye.')
 }
 
 startGame();
-
 
