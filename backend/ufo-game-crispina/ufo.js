9a2a30e5aad75812febf8bc22612744911b6b0c2
crispinamuriel <crispinamuriel@gmail.com>
2020-01-30 17:25:33 -0500

    fix: fixed bug with display of ufo[0]

--- backend/ufo-game-crispina/ufo.js
+++ backend/ufo-game-crispina/ufo.js
@@ -100,7 +100,7 @@ const x = [`                 .
 `];
 
 const y = [];
-for (let i = x.length - 1; i > 0; i--) {
+for (let i = x.length - 1; i >= 0; i--) {
     y.push(x[i])
 }
 module.exports = y;
